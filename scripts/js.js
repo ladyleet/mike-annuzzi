616414adea38454acfa4b1f73abec783b29d40af
Tracy Lee <tracy@wingman.events>
2015-05-16 20:22:09 -0700

    v1

---
+++ scripts/js.js
@@ -0,0 +1,19 @@
+// var myImage = document.getElementById("imageChange");
+// var imageArray = ["images/annuzzi-lba-2.jpg", "images/annuzzi-nature.jpg", "images/annuzzi-lba.jpg", "images/annuzzi-mtn-winery.jpg"];
+
+// var imageIndex = 0;
+
+// function changeImage(){
+// 	myImage.setAttribute("src", imageArray[imageIndex]);
+// 	imageIndex++;
+// 	if (imageIndex >= imageArray.length) {
+// 		imageIndex = 0;
+// 	}
+// }
+
+// var intervalHandle = setInterval(changeImage, 5000);
+
+ $(document).ready(function(){
+      $('.slider').slider({full_width: true});
+    });
+        
