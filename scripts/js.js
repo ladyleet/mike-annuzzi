114f10ffeda47d231fc16b1c2c96ea0ec37ccbdc
Tracy Lee <tracy@wingman.events>
2015-05-17 00:45:39 -0700

    fix-media-player-add-mobile-nav

--- scripts/js.js
+++ scripts/js.js
@@ -13,10 +13,16 @@
 
 // var intervalHandle = setInterval(changeImage, 5000);
 
- $(document).ready(function(){
-      $('.slider').slider({full_width: true});
-    });
-        
+$(document).ready(function(){
+	setTimeout(function () {
+		$('.slider').slider({full_width: true});
+	}, 200);
+});
+
+// var songs = {
+// 	'../myfile.mp3': new Audio('../myfile.mp3');
+// };
+var songs = {};
 
 function clickButton (){
 $(".accordion-content").toggleClass("open");
@@ -26,8 +32,36 @@ function clickButtonShows (){
 $(".accordion-content-shows").toggleClass("open");
 };
 
-function playSong(songFileName){
-	var song = new Audio(songFileName)
-	song.play();
+function playSong(playPauseButton, songFileName){
+	var $playPauseButton = $(playPauseButton);
+	var song = songs[songFileName] ? songs[songFileName].song : null;
+
+	if (!song) {
+		song = new Audio(songFileName);
+		songs[songFileName] = {song: song, elem: $playPauseButton};
+	}
+
+	if ($playPauseButton.hasClass("mdi-av-play-arrow")){
+		$playPauseButton.removeClass("mdi-av-play-arrow");
+		$playPauseButton.addClass("mdi-av-pause");
+		song.play();
+		
+		for(var s in songs) {
+			if (!songs[s].song.paused && s !== songFileName) {
+				songs[s].elem.removeClass("mdi-av-pause");
+				songs[s].elem.addClass("mdi-av-play-arrow");
+				songs[s].song.pause();
+			}
+		}
+	}
+
+	else {
+		$playPauseButton.removeClass("mdi-av-pause");
+		$playPauseButton.addClass("mdi-av-play-arrow");
+		song.pause();
+	}
 }
+
+// MOBILE COLLAPSE
+   $(".button-collapse").sideNav();
 
